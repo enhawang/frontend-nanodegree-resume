67c7b9602a70dee20b42902f770ffbc853a114f4
enhawang <nawang.yolmo@gmail.com>
2017-03-09 23:12:24 +0530

    Experiment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#header").append();
- $("#main").append("Nawang");
+ $("#main").append();
+ var name= "Nawang";
+ var value= 32;
+ console.log(name);
+ var awesomeThoughts = "My Name is Nawang and I am AWESOME!";
+ console.log(awesomeThoughts);
+ var funThoughts=awesomeThoughts.replace("AWESOME!", "FUN");
+ $("#main").append(funThoughts);
