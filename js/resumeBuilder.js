be28d8e0d744f09f8e09af4e92c225d50b37095e
enhawang <nawang.yolmo@gmail.com>
2017-03-10 00:20:25 +0530

    BIO

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,4 +9,21 @@ This is empty on purpose! Your code to build the resume will go here.
  var awesomeThoughts = "My Name is Nawang and I am AWESOME!";
  console.log(awesomeThoughts);
  var funThoughts=awesomeThoughts.replace("AWESOME!", "FUN");
- $("#main").append(funThoughts);
+ var formattedName= HTMLheaderName.replace("%data%", name);
+ $("#header").prepend(formattedName);
+ var formattedRole= HTMLheaderRole.replace("%data%", "Web & Android Developer");
+ $("#header").append(formattedRole);
+ var bio={
+ 	"name":"Nawang",
+ 	"role":"Web and Android Developer",
+ 	"contacts": "nawang.yolmo@gmail.com",
+ 	"welcome_message": "Welcome, bitch!",
+ 	"skills":["coding", "cycling", "writing", "painting"],
+ 	"bioPic":"images/me.jpg"
+ }
+ $("#main").append(bio.bioPic);
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contacts);
+$("#main").append(bio.welcome_message);
+$("#main").append(bio.skills);
